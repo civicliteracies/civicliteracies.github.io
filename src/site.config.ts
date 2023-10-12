b21f6b1a51f0d4525f467cb1cfce2acbac5916cd
benhur07b <benhur07b@users.noreply.github.com>
2023-10-12 19:04:43 +0800

    homepage: update size of text at large screen sizes; add tagline with changing texts

--- src/site.config.ts
+++ src/site.config.ts
@@ -6,7 +6,7 @@ export const siteConfig: SiteConfig = {
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
 	title: "Civic Literacy Initiative",
 	// Meta property used as the default description meta property
-	description: "Capacity building with care",
+	description: "Capacity-building with care",
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: "en-US",
 	// Meta property, found in src/components/BaseHead.astro L:42
